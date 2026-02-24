bf2944e0484b8c1b0a947d41e5546e6cc30b1bc4
Kmitl68070131 <68070131@kmitl.ac.th>
2026-02-24 23:32:24 +0700

    update Game.java
    ทำjframeคราวๆ

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,12 +1,52 @@
 package main;
 
-import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import javax.swing.*;
+import java.awt.*;
 
-public class Game extends JPanel implements Runnable {
-    JFrame frame_main = new JFrame;
+public class Game {
 
+    JFrame frame_main;
+    JButton btn_close;
+
+    public Game() {
+
+        frame_main = new JFrame("Amonghack");
+        frame_main.setUndecorated(true);
+        frame_main.setSize(380, 120);
+        frame_main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame_main.setLayout(null);
+
+        ImageIcon originalIcon = new ImageIcon(getClass().getResource("close.png"));
+
+        int originalWidth = originalIcon.getIconWidth();
+        int originalHeight = originalIcon.getIconHeight();
+
+        int newWidth = 50;
+        int newHeight = (originalHeight * newWidth) / originalWidth;
+
+        Image scaledImage = originalIcon.getImage()
+                .getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+        btn_close = new JButton(scaledIcon);
+
+        btn_close.setBounds(frame_main.getWidth() - newWidth - 10, 10, newWidth, newHeight);
+
+        btn_close.setBorderPainted(false);
+        btn_close.setContentAreaFilled(false);
+        btn_close.setFocusPainted(false);
+        btn_close.setOpaque(false);
+
+        btn_close.addActionListener(e -> System.exit(0));
+
+        frame_main.add(btn_close);
+
+        frame_main.setLocationRelativeTo(null);
+        frame_main.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Game();
+    }
 }
