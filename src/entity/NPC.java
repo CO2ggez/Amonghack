52ebba23928f3d347b5e92aafb061ed4a0dcd18d
Peerapatco2 <68070128@kmitl.ac.th>
2026-03-13 22:52:58 +0700

    โครง Dialog
    โครง dialog ต่อไปใส่บท dialog

--- src/entity/NPC.java
+++ src/entity/NPC.java
@@ -1,4 +1,24 @@
 package entity;
 
 public class NPC {
+    protected int x;
+    protected int y;
+    protected String[] dialogues;
+
+    public NPC(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String[] getDialogues() {
+        return dialogues;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
