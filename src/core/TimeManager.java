c282813dc8771a004437c82cde0e5f897720c578
Peerapatco2 <68070128@kmitl.ac.th>
2026-03-22 11:38:29 +0700

    Update TimeManager.java

--- src/core/TimeManager.java
+++ src/core/TimeManager.java
@@ -33,7 +33,7 @@ public class TimeManager implements Runnable {
     public void run() {
         while (running) {
             try {
-                Thread.sleep(5); //ค่าปกติ 2000 เทสใช้ 5 ให้ time มันวิ่งไว
+                Thread.sleep(2000); //ค่าปกติ 2000 เทสใช้ 5 ให้ time มันวิ่งไว
                 if (!isPaused && !isDayEnded) {
                     inGameMinutes++;
                     if (inGameMinutes >= 360) {
