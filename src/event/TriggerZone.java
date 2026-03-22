702eb11420d07dc3febc53b502ce3c573fca7e21
Pleumderja <68070148@kmitl.ac.th>
2026-03-22 20:20:06 +0700

    Update TriggerZone.java

--- src/event/TriggerZone.java
+++ src/event/TriggerZone.java
@@ -1,4 +1,23 @@
 package event;
 
 public class TriggerZone {
+    private String name;
+    private int x, y, width, height;
+
+    public TriggerZone(String name, int x, int y, int width, int height) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean isHit(int mouseX, int mouseY) {
+        return (mouseX >= x && mouseX <= x + width) &&
+               (mouseY >= y && mouseY <= y + height);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
