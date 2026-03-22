bd9e3ed805230e2fa587a5fd2c6f35fe276c7e6c
Pleumderja <68070148@kmitl.ac.th>
2026-03-22 20:20:17 +0700

    Update EventManager.java

--- src/event/EventManager.java
+++ src/event/EventManager.java
@@ -1,4 +1,51 @@
 package event;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventManager {
+    private List<TriggerZone> zones;
+    private boolean showImage;
+    private String activeZoneName;
+
+    public EventManager() {
+        zones = new ArrayList<>();
+        showImage = false;
+        activeZoneName = "";
+    }
+
+    public void addZone(TriggerZone zone) {
+        zones.add(zone);
+    }
+
+    public void checkClick(int mouseX, int mouseY) {
+        boolean clickedOnZone = false;
+
+        for (TriggerZone zone : zones) {
+            if (zone.isHit(mouseX, mouseY)) {
+                if (showImage && activeZoneName.equals(zone.getName())) {
+                    showImage = false;
+                    activeZoneName = "";
+                } else {
+                    showImage = true;
+                    activeZoneName = zone.getName();
+                }
+                clickedOnZone = true;
+                break;
+            }
+        }
+
+        if (!clickedOnZone) {
+            showImage = false;
+            activeZoneName = "";
+        }
+    }
+
+    public boolean isShowImage() {
+        return showImage;
+    }
+
+    public String getActiveZoneName() {
+        return activeZoneName;
+    }
 }
