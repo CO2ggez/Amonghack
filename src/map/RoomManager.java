c4814039f8325527b947f2ad63ca05f7173fce9c
Pleumderja <68070148@kmitl.ac.th>
2026-03-23 13:07:19 +0700

    เปลี่ยนชั้นกดF

--- src/map/RoomManager.java
+++ src/map/RoomManager.java
@@ -10,10 +10,10 @@ public class RoomManager {
     private MapLoader mapLoader;
     private Player player;
 
-    private String currentRoom;
-    private String[] mapDataFloor2 = {"lift2","office","itsupport","server","stair2"}; //กะว่าจะให้เก็บข้อมูลการเชื่อมโยงของแต่ละห้องในนี้
-    private String[] mapDataFloor1 = {"lift1","art","market","chiefoffice","stair1"};
-    private String[] mapDataFloorG = {"liftG","restroom","meeting","stairG"};
+    public String currentRoom;
+    public String[] mapDataFloor2 = {"lift2","office","itsupport","server","stair2"}; //กะว่าจะให้เก็บข้อมูลการเชื่อมโยงของแต่ละห้องในนี้
+    public String[] mapDataFloor1 = {"lift1","art","market","chiefoffice","stair1"};
+    public String[] mapDataFloorG = {"liftG","restroom","meeting","stairG"};
 
     private String[] currentFloor = mapDataFloor1;
     private int currentRoomIndex = 0;
@@ -41,6 +41,27 @@ public class RoomManager {
             player.xDelta = mapLoader.getWidth()-160;
             player.checkRight = false;
         }
+    }
+
+    // เมธอดสำหรับเปลี่ยนชั้น
+    public void changeFloor(String[] targetFloor, String targetRoomName, int newX) {
+        this.currentFloor = targetFloor;
+        // หาว่าห้องบันไดที่ไปถึง อยู่ลำดับที่เท่าไหร่ในชั้นใหม่
+        for (int i = 0; i < currentFloor.length; i++) {
+            if (currentFloor[i].equals(targetRoomName)) {
+                currentRoomIndex = i;
+                break;
+            }
+        }
+        // โหลดแมพใหม่
+        mapLoader = new MapLoader(currentFloor[currentRoomIndex]);
+        // วางตัวละครไว้ที่จุดบันไดของชั้นใหม่
+        player.xDelta = newX;
+    }
+
+    // เพิ่ม Getter เพื่อให้ InputManager เช็คชื่อห้องปัจจุบันได้
+    public String getCurrentRoomName() {
+        return currentFloor[currentRoomIndex];
     }
 
     public void drawMap(Graphics g, Camera camera) {
