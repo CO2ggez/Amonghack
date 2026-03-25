775b28e3820bbe72a028bf4bca46d454f9ada6a6
AIMVIEW <120380328+AIMVIEW@users.noreply.github.com>
2026-03-25 16:42:49 +0700

    เพิ่ม MinigameManager

--- src/network/LanCable.java
+++ src/network/LanCable.java
@@ -1,4 +1,22 @@
 package network;
 
-public class LanCable {
+import core.GamePanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class LanCable implements Minigame{//underconstruction ครับ
+    private JPanel panel;
+    private MinigameManager manager;
+
+    public LanCable(MinigameManager manager) {
+        this.manager = manager;
+
+    }
+
+    @Override
+    public JPanel getPanel() {
+        return panel;
+    }
 }
