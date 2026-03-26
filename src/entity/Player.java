743b8f079dc0546775da117b424c08dd7d9c8cfd
Kmitl68070131 <68070131@kmitl.ac.th>
2026-03-26 14:23:37 +0700

    fix speed animation player

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -28,7 +28,7 @@ public class Player extends JPanel {
     //เราเพิ่มความเร็วในการเดิน
     private int speed = 7;
     private int aniTick = 0;
-    private int aniSpeed = 15;
+    private int aniSpeed = 10;
 
     //แก้ไม่ให้ตัวละครเดินแล้วดูกระตุก ตอนอยู่กลางกล้อง
     public boolean leftPressed = false;
