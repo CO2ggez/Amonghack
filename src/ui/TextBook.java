f13c3407a8fa5296030f1f20ccde2e138c9b9a9e
kopiortys-v <kopiortys@gmail.com>
2026-03-27 13:33:00 +0700

    adding the update method textbook

--- src/ui/TextBook.java
+++ src/ui/TextBook.java
@@ -8,21 +8,33 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class TextBook extends JPanel {
+    public int update=0;
     public BufferedImage image;
     public TextBook() throws IOException {
         setSize(300,300);
         //addKeyListener(new InputManager()); ขออณุญาติย่ายไปสร้างใน gamePanel แทน เนื่องจากระบบเปลี่ยนห้องจะใช้ด้วย
 
-        importImg();
+        importImg("/util/asst/textbook_original.png");
 
     }
-    public void importImg() throws IOException {
+    public void importImg(String path) throws IOException {
         //use this as the prototype for other textbook file using if else Changing textbook file when a certain event
         //happened
-        InputStream is = getClass().getResourceAsStream("/util/asst/Textbook.png");
+        InputStream is = getClass().getResourceAsStream(path);
         image = ImageIO.read(is);
     }
-
+    public void update() throws IOException{
+        update+=1;
+        if (update==1){
+            importImg("/util/asst/textbook_2.png");
+        }if(update==2){
+            importImg("/util/asst/textbook_3.png");
+        }if(update==3){
+            importImg("/util/asst/textbook_4.png");
+        }if(update==4){
+            importImg("/util/asst/textbook_5.png");
+        }
+    }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
