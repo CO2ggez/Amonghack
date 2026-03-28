934d588974fd6be2a5ce6b0d94861428340cc6cc
AIMVIEW <120380328+AIMVIEW@users.noreply.github.com>
2026-03-28 22:16:04 +0700

    แก้ lauren เดินเอง day2

--- src/state/Day1State.java
+++ src/state/Day1State.java
@@ -126,9 +126,8 @@ public class Day1State extends AbstractState {
         if(gamePanel.getInputManager().talkedToHR == true && hrWalked == false) {
             if (gamePanel.dialogBox == null || !gamePanel.dialogBox.isVisible()){
                 hrWalked = true;
-                gamePanel.getNpcmanager().hr.moveTo(-3200);
+                gamePanel.getNpcmanager().hr.moveTo(-320);
             }
-
         }
 
         h = this.gamePanel.timeManager.getHours();
